b2dcdfacf97aa87903c7c4a9dfcd1102e403f804
Sasha5685 <125757473+Sasha5685@users.noreply.github.com>
2026-06-15 01:00:22 +0300

    fafa
    afaf

--- 3DSyzetSuperIgra/Assets/Scenes/FindHiddenObjectsWindow.cs
+++ 3DSyzetSuperIgra/Assets/Scenes/FindHiddenObjectsWindow.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
@@ -55,3 +56,4 @@ public class FindHiddenObjectsWindow : EditorWindow
         EditorUtility.DisplayDialog("Результат", $"Найдено скрытых объектов: {hiddenObjects.Count}\nИмена выведены в Console.", "OK");
     }
 }
+#endif
